353c63123c189672ad5fa1b64db53fa5e9754661
Nalin Prabhath <48054155+nalin2002@users.noreply.github.com>
2026-08-23 16:16:15 -0500

    Time: 0 ms (100.00%) | Memory: 8.6 MB (54.58%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int reverse(int x) {
+        if(x==0) return 0;
+
+        int rev=0;
+        while(x){
+            int rem= x%10;
+            x= x/10;
+
+            if( rev> INT_MAX/10 || (rev==INT_MAX/10 && rem>7)){
+                return 0;
+            }
+            if(rev< INT_MIN/10 || (rev==INT_MIN/10 && rem<-8)){
+                return 0;
+            }
+            rev= rev*10+rem;
+        }
+        return rev;
+    }
+};
